7859e0972343dbecab4f4580b17903d11f51e5a9
Gerald Todd <beingforthebenefit@gmail.com>
2024-07-28 14:58:07 -0700

    Dockerize and add paths

--- frontend/src/components/RegisterEvent.tsx
+++ frontend/src/components/RegisterEvent.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import {gql, useMutation} from '@apollo/client'
 import {Container, TextField, Button, Typography, Box} from '@mui/material'
 import {useNotification} from '../contexts/NotificationContext'
-import { REGISTER_EVENT } from 'src/queries/eventQueries'
+import { REGISTER_EVENT } from '../queries/eventQueries'
 
 const RegisterEvent: React.FC = () => {
   const [eventId, setEventId] = useState('')
